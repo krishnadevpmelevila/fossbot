03eecc0de75fcad5367971b5aafca3ed9f218d3f
Krishnadev P Melevila <krishnadev@kali>
2022-06-10 23:16:47 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,180 @@
+require('dotenv').config();
+const axios = require('axios');
+
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const port = 3001
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`BOT app listening at http://localhost:${port}`)
+})
+const { Client, Intents, Role } = require('discord.js');
+
+const bot = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_MESSAGES]
+});
+
+bot.on('ready', () => {
+  console.log(`Bot ${bot.user.tag} is logged in!`);
+
+
+});
+
+bot.login(process.env.BOT_TOKEN); // Replace the macro with your token
+
+bot.on('guildMemberAdd', (member) => {
+  // console.log(member)
+});
+// Welcome Message 
+bot.on('guildMemberAdd', (member) => {
+  member.roles.add('984841687522238504');
+
+});
+bot.on('messageCreate', (message) => {
+  console.log("ok");
+  if (message.author.id != '907633615599452160' && message.author.id != '282859044593598464' && message.author.id != '270904126974590976' && message.author.id != '707101869964656721' && message.content.toLowerCase() != 'clearchat' && message.channel.id != '984852138599534752'&&message.channel.id != '984816997420396607') {
+    console.log("ok");
+    axios.post(process.env.API_URL + '/login', {
+
+
+      email: process.env.USER_NAME,
+      password: process.env.PASS
+
+
+    }).then(function(response) {
+      axios.get(process.env.API_URL + '/data', {
+        headers: {
+          'x-access-token': response.data.token
+        }
+      }).then(function(response) {
+        hasMatch = false;
+        var data = response.data
+        for (var index = 0; index < data.length; ++index) {
+
+          var user = data[index];
+          console.log(user.discordid);
+          if (user.discordid == message.author.id && user.activated == true) {
+
+            hasMatch = true;
+            break;
+          }
+
+        }
+        if (!hasMatch) {
+          if (message.member != null) {
+            message.member.roles.add('984841687522238504');
+            message.member.roles.remove('984841332013031494');
+            message.author.send(`<@${message.author.id}> Please verify your account first. Check your email for the verification code.  If you are once verified and now seeing this message, Then it seems the admin had deactivated your membership. Check the reason with <@707101869964656723>`)
+            message.delete();
+          }
+
+        }
+      })
+
+
+
+    })
+  }
+})
+
+
+bot.on('messageCreate', (message) => {
+  if (message.author.id == '707101869964656723' && message.author.id != '282859044593598464' && message.author.id != '270904126974590976') {
+    if (message.content.toLowerCase().startsWith("clearchat")) {
+      async function clear() {
+
+        message.channel.bulkDelete(100);
+        message.delete();
+
+      }
+      clear();
+    }
+  }
+
+  if (message.channelId == '984852138599534752' && message.author.id != '984838832656633856' && message.author.id != '907633615599452160' && message.author.id != '270904126974590976' && message.content.toLowerCase() != 'clearchat') {
+
+
+    axios.post(process.env.API_URL + '/login', {
+
+
+      email: process.env.USER_NAME,
+      password: process.env.PASS
+
+
+    }).then(function(response) {
+      var token = response.data.token
+      axios.get(process.env.API_URL + '/data', {
+        headers: {
+          'x-access-token': token
+        }
+      }).then(function(response) {
+
+        // console.log(message.author.id)
+        // loop
+        var data = response.data
+        var i;
+        var flag = false;
+
+        // console.log(data);
+        var hasMatch = false;
+
+        for (var index = 0; index < data.length; ++index) {
+
+          var user = data[index];
+
+          if (user.activationCode == message.content) {
+            hasMatch = true;
+            break;
+          }
+        }
+        if (hasMatch) {
+          axios.get(process.env.API_URL + '/activate/' + user._id, {
+            headers: {
+              'x-access-token': token
+            }
+          }).then(function(response) {
+            usersname = user.name
+            axios.get(process.env.API_URL + '/add/' + user._id + '/' + message.author.id, {
+              headers: {
+                'x-access-token': token
+              }
+            }).then(function(response) {
+              message.reply(usersname + ', You have been verified!, IMPORTANT: Dont forget to enter this code on quizess platfrom while it is asked! All the best!')
+              //  add role
+              console.log(message.content);
+              message.member.roles.add('984841332013031494');
+              message.member.roles.remove('984841687522238504');
+              
+
+            }
+            )
+
+          })
+        } else {
+          message.reply("Invalid Activation Code")
+          
+        }
+
+      }).catch(function(error) {
+
+        console.log(error);
+      }
+      );
+    }
+    ).catch(function(error) {
+      console.log(error);
+    });
+
+
+
+  }
+
+});
+
+
+
